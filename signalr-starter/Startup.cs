c19561ed8f6f23241a0c533522984fc95cbe90ec
Jeremy Gaerke <jeremy.gaerke@pinnsg.com>
2016-03-15 23:11:19 -0400

    simple message hub setup

---
+++ signalr-starter/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(signalr_starter.Startup))]
+
+namespace signalr_starter
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
